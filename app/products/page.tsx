6b661d23dc0995735ec9d11bac08265cae537b1d
powiedl <159603185+powiedl@users.noreply.github.com>
2024-12-08 18:16:32 +0100

    also use @ts-expect-error Server Component in products/page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,6 +8,7 @@ const ProductsPage = ({
   const layout = searchParams.layout || 'grid';
   const search = searchParams.search || '';
   console.log('ProductsPage', searchParams);
+  /* @ts-expect-error Server Component */
   return <ProductsContainer layout={layout} search={search} />;
 };
 export default ProductsPage;
